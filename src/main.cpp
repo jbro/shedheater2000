1c7884ca0c16f198145bda494f4c2b198258c08d
Jesper B. Rosenkilde <jbr@humppa.dk>
2025-10-03 01:00:24 +0200

    Refactor
    - Push clear button during boot to clear wifi credentials
    - Portal always on
    - Status page with temp, time and params
    - time from NTP

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <WiFiManager.h>
 #include <ESP8266mDNS.h>
 #include <thermistor.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 #define FAN_PIN D2
-#define CONFIG_BTN_PIN D5
+#define CLEAR_BTN_PIN D5
 #define HEATER_1_PIN D6
 #define HEATER_2_PIN D7
 #define THERMISTOR_PIN A0
@@ -12,15 +14,40 @@
 WiFiManager wm;
 
 unsigned int wifiPortalTimeout = 180;
-unsigned long wifistartTime = millis();
 
 bool portalRunning = false;
 bool startAP = false;
 
-// Function prototype
-void doWiFiManager();
+class IntParameter : public WiFiManagerParameter
+{
+public:
+  IntParameter(const char *id, const char *placeholder, long value, const uint8_t length = 10)
+      : WiFiManagerParameter("")
+  {
+    init(id, placeholder, String(value).c_str(), length, "", WFM_LABEL_BEFORE);
+  }
+
+  long getValue()
+  {
+    return String(WiFiManagerParameter::getValue()).toInt();
+  }
+
+  String getValueStr()
+  {
+    return String(WiFiManagerParameter::getValue());
+  }
+};
+
+// Custom Parameters
+IntParameter setpointParam("setpointParam", "Temperature Setpoint (C)", 5);
+IntParameter hysteresisParam("hysteresisParam", "Hysteresis (C)", 1);
+IntParameter fanRunOnParam("fanRunOnParam", "Fan Run On Time (seconds)", 60);
+WiFiManagerParameter ntpServerParam("ntpServerParam", "NTP Server", "pool.ntp.org", 32);
 
 Thermistor *thermistor;
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntpServerParam.getValue(), 0, 3600); // Update every hour
 
 void setup()
 {
@@ -39,7 +66,14 @@ void setup()
 
   pinMode(THERMISTOR_PIN, INPUT);
 
-  pinMode(CONFIG_BTN_PIN, INPUT_PULLUP);
+  pinMode(CLEAR_BTN_PIN, INPUT_PULLUP);
+
+  // Forget wifi credentials
+  if (digitalRead(CLEAR_BTN_PIN) == LOW)
+  {
+    Serial.println("Button held on startup, clearing WiFi credentials");
+    wm.resetSettings();
+  }
 
   // Setup Thermistors
   thermistor = new Thermistor(THERMISTOR_PIN, 3.3, 3.3, 1023, 10000, 10000, 27.8, 3380, 1, 0);
@@ -54,6 +88,7 @@ void setup()
 
   wm.setHostname("shedheater2000");
   MDNS.begin("shedheater2000");
+
   wm.setConfigPortalTimeout(wifiPortalTimeout);
   wm.autoConnect("shedheater2000-Setup");
 
@@ -64,57 +99,69 @@ void setup()
     digitalWrite(LED_BUILTIN, HIGH);
     delay(200);
   }
+
+  // Set up custom menu
+  const char *menu[] = {"custom", "param", "info"};
+  wm.setMenu(menu, sizeof(menu) / sizeof(menu[0]));
+
+  // Add your custom menu HTML (e.g., a link to /status)
+  wm.setCustomMenuHTML("<form action='/status' method='get'><button>Status</button></form><br />");
+
+  // Add custom parameters
+  wm.addParameter(&setpointParam);
+  wm.addParameter(&hysteresisParam);
+  wm.addParameter(&fanRunOnParam);
+  wm.addParameter(&ntpServerParam);
+
+  // TODO Restore parameter values from EEPROM
+  // TODO register a callback to save the custom parameters to EEPROM
+  // XXX don't save ntp server param to eeprom
+  // TODO implement hysteresis and fan run on time
+
+  // Add custom routes to WiFiManager's web server
+  wm.setWebServerCallback([&]()
+                          { wm.server->on("/status", HTTP_GET, [=]()
+                                          {
+      float temp = thermistor->readTempC();
+      String setPoint = setpointParam.getValueStr();
+      String hysteresis = hysteresisParam.getValueStr();
+      String fanTimeout = fanRunOnParam.getValueStr();
+
+      // XXX Move time and temperature into their own pages and update with JS
+      String page;
+      page += FPSTR(HTTP_HEAD_START);
+      page.replace(FPSTR(T_v), "Status");
+      page += FPSTR(HTTP_SCRIPT);
+      page += FPSTR(HTTP_STYLE);
+      page += FPSTR(HTTP_HEAD_END);
+      page += FPSTR(HTTP_ROOT_MAIN);
+      page.replace(FPSTR(T_t), "ShedHeater2000");
+      page.replace(FPSTR(T_v), "Status");
+      page += "<p>Current Time: " + timeClient.getFormattedTime() + " UTC</p>";
+      page += "<p>Temperature: " + String(temp, 1) + " &deg;C</p>";
+      page += "<p>Setpoint: " + setPoint + " &deg;C</p>";
+      page += "<p>Hysteresis: " + hysteresis + " &deg;C</p>";
+      page += "<p>Fan Run On Time: " + fanTimeout + " seconds</p>";
+      page += FPSTR(HTTP_BR);
+      page += FPSTR(HTTP_BACKBTN);
+      page += FPSTR(HTTP_END);
+      wm.server->send(200, "text/html", page); }); });
 }
 
 void loop()
 {
+  // Update NTP Client
+  timeClient.update();
+
   // Send mDNS update
   MDNS.update();
 
+  // Make sure the web portal is running if requested
+  if (!wm.getConfigPortalActive() && !wm.getWebPortalActive())
+  {
+    wm.startWebPortal();
+  }
+
   // Handle WiFi Manager
-  doWiFiManager();
-}
-
-void doWiFiManager()
-{
-  // is auto timeout portal running
-  if (portalRunning)
-  {
-    wm.process();
-
-    // check for timeout
-    if ((millis() - wifistartTime) > (wifiPortalTimeout * 1000))
-    {
-      Serial.println("portal timed out");
-      portalRunning = false;
-      digitalWrite(LED_BUILTIN, HIGH);
-      if (startAP)
-      {
-        wm.stopConfigPortal();
-      }
-      else
-      {
-        wm.stopWebPortal();
-      }
-    }
-  }
-
-  // is configuration portal requested?
-  if (digitalRead(CONFIG_BTN_PIN) == LOW && (!portalRunning))
-  {
-    if (startAP)
-    {
-      Serial.println("Button Pressed, Starting Config Portal");
-      wm.setConfigPortalBlocking(false);
-      wm.startConfigPortal();
-    }
-    else
-    {
-      Serial.println("Button Pressed, Starting Web Portal");
-      wm.startWebPortal();
-    }
-    portalRunning = true;
-    digitalWrite(LED_BUILTIN, LOW);
-    wifistartTime = millis();
-  }
+  wm.process();
 }
